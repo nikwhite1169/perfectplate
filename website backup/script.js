0621ebed70147e8d66dd044dff3289bd49449a9a
nikwhite1169 <nikpwhite@gmail.com>
2025-01-14 03:14:57 -0800

    Removed app - Copy directory

--- website backup/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-    const body = document.body;
-    if (window.scrollY > 50) {
-      body.classList.add('scrolled');
-    } else {
-      body.classList.remove('scrolled');
-    }
-  });
